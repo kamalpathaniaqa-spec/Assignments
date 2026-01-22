59a0b68f5616d279af07fc1c26b2baa564f319e9
kamalpathaniaqa-spec <Kamalpathania.qa@gmail.com>
2026-01-22 21:36:17 +0530

    Add assignment7.ts

--- assignments/assignment7.ts
+++ assignments/assignment7.ts
@@ -1,24 +1,32 @@
 // Best Time to Buy and Sell Stock
 
+function maxProfitStock(prices: number[]): number
+  let maxProfit: number = 0;
+  let buyDay: number = 0;
+  let sellDay: number = 0;
 
-function maxProfit(prices: number[]): number {
-    let minPrice = Infinity;
-    let maxProfit = 0;
+  // Loop through each day
+  for (let i: number = 0; i < prices.length - 1; i++) {
+    for (let j: number = i + 1; j < prices.length; j++) {
+      const profit: number = prices[j] - prices[i];
 
-    for(let price of prices){
-        if(price < minPrice){
-
-        minPrice = price;
-        
-    } else if (price - minPrice > maxProfit){
-      maxProfit = price - minPrice;
+      // Correct comparison
+      if (profit > maxProfit) {
+        maxProfit = profit;
+        buyDay = i + 1;   // 1-based index
+        sellDay = j + 1;
+      }
     }
-} 
-return maxProfit;
-}
-   //calling the function
-   console.log(maxProfit([7, 1, 5, 3, 6, 4])); 
-    console.log(maxProfit([7, 6, 4, 3, 1])); 
+  }
+
+  // Print result
+  if (maxProfit > 0) {
+    console.log("Max Profit is " + maxProfit);
+    console.log("Buy the share on day " + buyDay);
+    console.log("Sell the share on day " + sellDay);
+  } else {
+    console.log("You will lose your money, don't buy this share");
+  }
 
 
-  export{};
+export{};
